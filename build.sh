612fd05e85d91a9c6227992015f3f1bfdaf28f0a
Alexander Krikun <krikun98@gmail.com>
2022-01-07 16:57:20 +0300

    Workflow draft

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 container_cmd="docker run -v=$(pwd):/kikit -w=/kikit --rm"
 
- mkdir -p gerbers
- mkdir -p images
-
+# Images
+mkdir -p images
 for name in "pcb" "plate" "bottom"
 do
 	for option in "$name"/*/
@@ -16,6 +15,8 @@ do
 	done
 done
 
+# Gerbers
+mkdir -p gerbers
 for name in "pcb"
 do
 	for option in "$name"/*/
@@ -28,18 +29,20 @@ do
 	done
 done
 
-
+# Plate/bottom dxf files
 mkdir -p dxf
-
 for name in "plate" "bottom"
+do
+for option in "$name"/*/
 	do
-	for option in "$name"/*/
-		do
-			short_option="$(basename "$option")"
-			file="$(find $option -type f -name '*.kicad_pcb')"
-			file_name=$(basename "$file" .kicad_pcb)
-			${container_cmd} yaqwsx/kikit:nightly kikit export dxf "$file" dxf/"$name"_"$short_option"
-			mv dxf/"$name"_"$short_option"/"$file_name"-EdgeCuts.dxf dxf/"$name"_"$short_option".dxf
-			rm -r dxf/"$name"_"$short_option"
-		done
+		short_option="$(basename "$option")"
+		file="$(find $option -type f -name '*.kicad_pcb')"
+		file_name=$(basename "$file" .kicad_pcb)
+		${container_cmd} yaqwsx/kikit:nightly kikit export dxf "$file" dxf/"$name"_"$short_option"
+		mv dxf/"$name"_"$short_option"/"$file_name"-EdgeCuts.dxf dxf/"$name"_"$short_option".dxf
+		rm -r dxf/"$name"_"$short_option"
 	done
+done
+
+zip -jr dxf/case_files dxf/
+	
